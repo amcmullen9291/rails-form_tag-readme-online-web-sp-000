e0ea09afb2255dbf719cc07757e9795bc32cb6c5
anthony mcmullen <amcmullen9291@gmail.com>
2021-01-26 22:59:42 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   config.web_console.whitelisted_ips = '<IP address here>'
 
   protect_from_forgery with: :exception
+  
+    def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+  redirect_to posts_path
+  end
+
 end
